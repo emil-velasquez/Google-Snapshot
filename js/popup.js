4a1d83678489defd4084aa989cb819013f62910b
Emil Velasquez <evelas10@utexas.edu>
2022-01-15 21:04:21 -0600

    Added licenses to the website

--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,8 @@ document.getElementById("info-button").addEventListener("click", togglePopup);
 function togglePopup() {
     var popup = document.getElementById("myPopup");
     var socials = document.getElementById("social-link-container");
+    var license = document.getElementById("license-link");
     popup.classList.toggle("show");
     socials.classList.toggle("show");
+    license.classList.toggle("show");
 }
